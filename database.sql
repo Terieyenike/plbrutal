a586068312b9adabc8780bc070ddeeb7c7b228d8
Teri Eyenike <teyenike1@gmail.com>
2020-10-28 14:29:35 +0100

    update to the readme and database

--- database.sql
+++ database.sql
@@ -4,5 +4,5 @@ CREATE TABLE form (
   id BIGSERIAL NOT NULL PRIMARY KEY,
   officer_name VARCHAR(100) NOT NULL,
   amount INTEGER NOT NULL,
-  date_of_incident DATE NOT NULL,
+  date_of_incident DATE NOT NULL
 );
